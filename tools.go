fff9e67af84b2f8a6d162b793d1f67de4e58110d
Frederik Zipp <fzipp@gmx.de>
2024-02-11 13:51:25 +0100

    add missing copyright headers

--- tools.go
+++ tools.go
@@ -1,3 +1,7 @@
+// Copyright 2018 Frederik Zipp.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 //go:build tools
 
 package tools
